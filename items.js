e1bf635c45efc47749867042dff29b806d11e84e
Manuel Mercado <manuel.mercado15@gmail.com>
2021-07-07 20:45:38 -0400

    init commit

---
+++ items.js
@@ -0,0 +1,26 @@
+let menu3Button = document.getElementById('menu-3-button');
+let menu4Button = document.getElementById('menu-4-button');
+
+let menu3Dropdown = document.getElementById('menu-3');
+let menu4Dropdown = document.getElementById('menu-4');
+
+menu3Button.addEventListener('mouseover', displayMenuThree);
+menu4Button.addEventListener('mouseover', displayMenuFour);
+
+function displayMenuThree() {
+    menu3Dropdown.classList.add('show');
+    menu4Dropdown.classList.remove('show');
+}
+
+function displayMenuFour() {
+    menu4Dropdown.classList.add('show');
+    menu3Dropdown.classList.remove('show');
+}
+
+menu3Dropdown.addEventListener('mouseleave', closeMenus);
+menu4Dropdown.addEventListener('mouseleave', closeMenus);
+
+function closeMenus() {
+    menu3Dropdown.classList.remove('show');
+    menu4Dropdown.classList.remove('show');
+}
